4214e059c4496bd3d9cb6f04a6c70c9ad976bcc9
ochosteve <ochosteve08@gmail.com>
2024-02-24 17:36:03 +0100

    integrate swagger documentation and user registration

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -3,7 +3,11 @@ import { userRegistration } from "../service/user.service";
 
 export const RegisterUser = async (req: Request, res: Response) => {
   try {
+    console.log(req.body);
+    console.log(req.method)
+    console.log(req.url)
     const { email, password, username } = req.body;
+    
     if (!email || !password || !username) {
       return res.status(400).json("all fields are required");
     }
@@ -11,6 +15,8 @@ export const RegisterUser = async (req: Request, res: Response) => {
     const user = await userRegistration(email, password, username);
     if (user) {
       return res.status(200).json(user);
+    } else {
+      return res.status(404).json("user registration failed");
     }
   } catch (err) {
     console.error(err);
