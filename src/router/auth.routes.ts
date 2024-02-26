3d4663e3d8eb9c036966e299ca11445e053909ff
ochosteve <ochosteve08@gmail.com>
2024-02-26 19:35:53 +0100

    integrate logout feature

--- src/router/auth.routes.ts
+++ src/router/auth.routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { RegisterUser, LoginUser } from "../controller/auth.controller";
+import { RegisterUser, LoginUser, Logout } from "../controller/auth.controller";
 
 const router = express.Router();
 router.post("/register", RegisterUser);
-router.post("/login", LoginUser);
+router.post("/login", LoginUser); 
+router.get("/logout", Logout);
 
 export default router;
